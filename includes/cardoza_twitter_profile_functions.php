c2d5602e44554fda9c5620be9b59c4133ba8357f
vinoj.cardoza <vinoj.cardoza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-17 09:45:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cardoza-twitter-box/trunk@506429 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/cardoza_twitter_profile_functions.php
+++ includes/cardoza_twitter_profile_functions.php
@@ -62,36 +62,4 @@ function widget_cardoza_twitter_box($args){
 	echo $after_widget;
 }
 
-function cardoza_twitter_box_sc(){
-    $option_value = ctb_retrieve_options($opt_val);
-	?>
-	<script>
-	new TWTR.Widget({
-		version: 2,
-	  	type: 'profile',
-		rpp: <?php echo $option_value['ctb_no_of_tweets'];?>,
-	  	interval: <?php echo $option_value['ctb_no_of_tweets']*1000;?>,
-	  	width: <?php echo $option_value['ctb_width'];?>,
-	  	height: <?php echo $option_value['ctb_height'];?>,
-	  	theme: {
-		shell: {
-		  background: '<?php echo $option_value['ctb_shell_bg_color'];?>',
-		  color: '<?php echo $option_value['ctb_shell_text_color'];?>'
-		},
-		tweets: {
-		  background: '<?php echo $option_value['ctb_tweet_bg_color'];?>',
-		  color: '<?php echo $option_value['ctb_tweet_text_color'];?>',
-		  links: '<?php echo $option_value['ctb_links_color'];?>'
-		}
-	  },
-	  features: {
-		scrollbar: <?php echo $option_value['ctb_scrollbar'];?>,
-		loop: <?php echo $option_value['ctb_loop_results'];?>,
-		live: <?php echo $option_value['ctb_live'];?>,
-		behavior: '<?php echo $option_value['ctb_behavior'];?>'
-	  }
-	}).render().setUser('<?php echo $option_value['ctb_username'];?>').start();
-	</script>
-<?php
-}
 ?>
